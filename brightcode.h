f0275a815eee08f53bbc1480dd04932de20b6597
Qwark <givernaud.f@gmail.com>
2014-12-27 13:08:17 +0000

    IL MONQUE QUELQUE CHOSE A MA VIE

---
+++ brightcode.h
@@ -0,0 +1,9 @@
+#ifndef BRIGHTCODE_H_
+# define BRIGHTCODE_H_
+
+void	bc_write_char(char);
+void	bc_write_string(char *);
+void	bc_write_number(int);
+char	*bc_read_line(const int);
+
+#endif /* !BRIGHTCODE_H_ */
